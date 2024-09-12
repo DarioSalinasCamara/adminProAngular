cad4705b323854b9cf29fa6f3873c2f93dd61c39
DarioSalinasCamara <dariosalinascamara@gmail.com>
2024-09-13 00:22:34 +0200

    Rutas hijas y modulos, commit final

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styles: ``
 })
 export class LoginComponent {
 
