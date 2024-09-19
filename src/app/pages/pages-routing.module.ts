8ae7d44c3c262b8263c635d5230660f18a8d6321
DarioSalinasCamara <dariosalinascamara@gmail.com>
2024-09-19 22:38:31 +0200

    AccountSettings component y settingsService

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -4,6 +4,7 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
 
@@ -15,7 +16,7 @@ const routes: Routes = [
         { path: '', component: DashboardComponent },
         { path: 'progress', component: ProgressComponent },
         { path: 'grafica', component: Grafica1Component },
-        { path: '', pathMatch: 'full', redirectTo: '/dashboard'},
+        { path: 'account-settings', component: AccountSettingsComponent },
         ]
     }
   ];
