f99715bbe2db02d697dedc5265aa5ad7cb989c44
DarioSalinasCamara <dariosalinascamara@gmail.com>
2024-09-20 23:39:04 +0200

    sideBar service y routing del registro y login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent,
+    RegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   exports: [
     LoginComponent,
-    RegisterComponent,
+    RegisterComponent
   ]
 })
 export class AuthModule { }
