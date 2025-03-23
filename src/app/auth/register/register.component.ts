a2cbe5547d769cc85d26b971c083ac50ac487472
DarioSalinasCamara <dariosalinascamara@gmail.com>
2025-03-23 19:08:41 +0100

    registerFom register.components.ts y validaciones

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css'] 
 })
 export class RegisterComponent {
+
+  public formSubmitted = false;
+
+  public registerForm = this.fb.group({
+    nombre: ['asasasaa', Validators.required],
+    email: ['asasa@asavg.com', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    password2: ['', Validators.required],
+    terminos: [false, Validators.required]
+
+  })
+
+
+  constructor( private fb: FormBuilder){}
+
+  crearUsuario() {
+    this.formSubmitted = true;
+    console.log( this.registerForm.value );
+
+    if( this.registerForm.valid) {
+      console.log('ok');
+    } else {
+      console.log('ko')
+    }
+  }
+
+  campoNoValido( campo: string): boolean{
+
+    if( this.registerForm.get(campo).invalid && this.formSubmitted){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  aceptaTerminos() {
+    
+    return !this.registerForm.get('terminos').value && this.formSubmitted;
+
+  }
 
 }
